54cf985e85da9783814b7fbd96c999d72631feb6
sabkam08 <sabelokama@gmail.com>
2026-08-05 22:25:22 +0200

    C# mini project

---
+++ c#/miniprojects/helloworld/helloworld/Program.cs
@@ -0,0 +1,5 @@
+﻿//comments are like any othter language
+/*this is a multi-line comment
+   that can span multiple lines
+   and is useful for providing detailed explanations or documentation.*/ 
+Console.WriteLine("Hello, World!");
