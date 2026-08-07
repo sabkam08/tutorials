f76666944d055dc022f9907339c6a4a03ae980b5
sabkam08 <sabelokama@gmail.com>
2026-08-07 19:45:10 +0200

    Angular mini project

--- c#/miniprojects/helloworld/helloworld/Program.cs
+++ c#/miniprojects/helloworld/helloworld/Program.cs
@@ -1,5 +1,12 @@
 ﻿//comments are like any othter language
-/*this is a multi-line comment
-   that can span multiple lines
-   and is useful for providing detailed explanations or documentation.*/ 
+//
 Console.WriteLine("Hello, World!");
+
+string company1 = "Microsoft";
+string company2 = "Amazon";
+double yearsWorked = 5.54;
+
+Console.WriteLine($"The company I work for is: {company1}");
+Console.WriteLine("The company I would like to work for is:" + company2);
+
+yearsWorked = double.Parse(Console.ReadLine());
